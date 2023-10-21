6185db50f6ce190de90c812b3f5a22bdaac1f279
Arjun <arjunjot18@gmail.com>
2023-10-21 15:51:35 +0200

    tailwindcss

---
+++ src/views/SignUp.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+        <p class="text-5xl">signUp</p>
+  </div>
+</template>
+
+<script>
+// import the firestore instance and relevant methods | 
+// import db from '@/firestore/init.js';
+// import { collection, addDoc } from 'firebase/firestore';
+export default {
+
+    meathod:{
+        // async createUser(){
+        //     const colRef = collection(db, 'users');
+
+        //     const dataObj = {
+        //         firstName: 'testUser',
+        //         lastname: 'testUser',
+        //         dob: 2000,
+        //     }
+
+        //     const docRef = await addDoc(colRef, dataObj);
+        //     console.log("Document Created with id: " + docRef.id);
+        // }
+    },
+    created(){
+        // this.createUser();
+    }
+}
+</script>
