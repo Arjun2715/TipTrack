ec727caaca76767483daab185f11af3d3edc5084
Arjun <arjunjot18@gmail.com>
2023-10-29 22:58:14 +0100

    minorFix

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -24,7 +24,7 @@
                 v-model="this.username"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="username"
-                required
+                required=""
               />
             </div>
             <div>
@@ -40,7 +40,7 @@
                 v-model="this.email"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="name@company.com"
-                required
+                required=""
               />
             </div>
             <div>
@@ -56,7 +56,7 @@
                 v-model="this.password"
                 placeholder="••••••••"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                required
+                required=""
               />
             </div>
             <div>
@@ -81,7 +81,7 @@
                   aria-describedby="terms"
                   type="checkbox"
                   class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                  required
+                  
                 />
               </div>
               <div class="ml-3 text-sm">
